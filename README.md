7cc7ae3b153a9740807ad33570e6d76402e7f74c
Tom Brooke <tom.brooke@gmail.com>
2020-09-27 16:09:36 -0400

    Started move from gbls to TBlocks

---
+++ README.md
@@ -0,0 +1,84 @@
+# TBlocks Module
+
+This package is the beginning of a collection of standard includes for TrustBlocks.
+
+It provides `basic-questions-tblocks.yml` which
+creates some standard objects and fills in their variables.
+
+Attributes that are filled in include name, address, email, and phone number, using the
+standard Individual attribute names.
+
+To use (and add the objects `client`, `advocate`, and `adverse_parties`) 
+include `docassemble.gbls:basic-questions-gbls.yml`
+
+E.g., when you include `docassemble.gbls:basic-questions-gbls.yml`, you will have
+the following attributes available to you:
+
+* client
+* client.full_name()
+* client.name.first / client.name.middle / client.name.last / client.name.suffix
+* client.address.address, client.address.zip, etc / client.address_block()
+* client.phone_number
+* client.mobile_number
+* client.email
+* client.birth_date
+
+Name fields and email will also be available for `advocate`. adverse_parties will
+be a DAList of Persons, with adverse_party[i].name.text and adverse_party[i].full_name()
+available. Address of adverse parties is not parsed.
+
+# Integrating with TrustBlocks Server
+
+After you install this package on your Docassemble Server, create a new instruction
+block on the case page with the contents below.
+
+Replace both instances of MYDOCASSEMBLESERVER with the name of your Docassemble server (e.g., `docs.legalaid.org`)
+and replace the text MYAPIKEY with an [API key](https://docassemble.org/docs/api.html#manage_api) that you generate on your Docassemble
+profile. Get the API key by clicking on your email address, select Profile, and then 
+Other Settings | API key.
+
+You will see a new block on the page with the same interviews that you see when you visit
+/list on your Docassemble server.
+
+```
+<input type="hidden" id="da_api_url" value="https://MYDOCASSEMBLESERVER/api/list?key="/>
+<input type="hidden" id="da_api_key" value="MYAPIKEY" />
+<script type="text/javascript" src="https://MYDOCASSEMBLESERVER/packagestatic/docassemble.gbls/docassemble-ls.js">
+</script>
+
+<div class="book book_large" id="docassemble_container">
+    <div class="bookTabRow">
+        <ul class="tab_start_here htabs">
+        <li class="bookTabItem dynamicBookTabItem selected_book_tab">
+        <a class="select">Docassemble Interviews</a>
+        </li>
+        </ul>
+    </div>
+    <div class="bookPage">
+        <div class="dynamicBookContents tabs">
+                <div style="border: 0px; padding: 0px; margin: 0px;"><div class="form_container"><div class="listview_outer">
+                    <div id="interviews" class="datatable">
+                    </div>
+                </div></div></div>
+        </div>
+    </div>
+</div>
+```
+# Sending fields to Docassemble without displaying in Legal Server
+You can create a whole tab block of fields that won't display in Legal Server but are still available to Docassemble,
+by creating an instruction with the contents:
+```
+<div id="docassemble-fields"></div>
+```
+Somewhere in the tab block that you want to hide.
+
+# Listviews
+As of version .30, this module supports Legal Server Listviews. 
+Please check the ListView option "Show list title" in order to get labeled data into Docassemble. Every listview
+in the form section needs to be labeled for the module to be able to guess which label corresponds to which list view.
+
+# Filtering
+
+By default, interviews will be filtered to match the metadata tag equalling the program name.
+Any interview tagged "everyone" will also be displayed.
+
---
+++ setup.py
@@ -0,0 +1,60 @@
+import os
+import sys
+from setuptools import setup, find_packages
+from fnmatch import fnmatchcase
+from distutils.util import convert_path
+
+standard_exclude = ('*.pyc', '*~', '.*', '*.bak', '*.swp*')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
+    out = {}
+    stack = [(convert_path(where), '', package)]
+    while stack:
+        where, prefix, package = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                if os.path.isfile(os.path.join(fn, '__init__.py')):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                        stack.append((fn, '', new_package))
+                else:
+                    stack.append((fn, prefix + name + '/', package))
+            else:
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix+name)
+    return out
+
+setup(name='docassemble.tblocks',
+      version='0.0.10',
+      description=('Basic package for all TrustBlocks interviews'),
+      long_description='# TBlocks Module\r\n\r\nThis package is the beginning of a collection of standard includes for TrustBlocks.\r\n\r\nIt provides `basic-questions-tblocks.yml` which\r\ncreates some standard objects and fills in their variables.\r\n\r\nAttributes that are filled in include name, address, email, and phone number, using the\r\nstandard Individual attribute names.\r\n\r\nTo use (and add the objects `client`, `advocate`, and `adverse_parties`) \r\ninclude `docassemble.gbls:basic-questions-gbls.yml`\r\n\r\nE.g., when you include `docassemble.gbls:basic-questions-gbls.yml`, you will have\r\nthe following attributes available to you:\r\n\r\n* client\r\n* client.full_name()\r\n* client.name.first / client.name.middle / client.name.last / client.name.suffix\r\n* client.address.address, client.address.zip, etc / client.address_block()\r\n* client.phone_number\r\n* client.mobile_number\r\n* client.email\r\n* client.birth_date\r\n\r\nName fields and email will also be available for `advocate`. adverse_parties will\r\nbe a DAList of Persons, with adverse_party[i].name.text and adverse_party[i].full_name()\r\navailable. Address of adverse parties is not parsed.\r\n\r\n# Integrating with TrustBlocks Server\r\n\r\nAfter you install this package on your Docassemble Server, create a new instruction\r\nblock on the case page with the contents below.\r\n\r\nReplace both instances of MYDOCASSEMBLESERVER with the name of your Docassemble server (e.g., `docs.legalaid.org`)\r\nand replace the text MYAPIKEY with an [API key](https://docassemble.org/docs/api.html#manage_api) that you generate on your Docassemble\r\nprofile. Get the API key by clicking on your email address, select Profile, and then \r\nOther Settings | API key.\r\n\r\nYou will see a new block on the page with the same interviews that you see when you visit\r\n/list on your Docassemble server.\r\n\r\n```\r\n<input type="hidden" id="da_api_url" value="https://MYDOCASSEMBLESERVER/api/list?key="/>\r\n<input type="hidden" id="da_api_key" value="MYAPIKEY" />\r\n<script type="text/javascript" src="https://MYDOCASSEMBLESERVER/packagestatic/docassemble.gbls/docassemble-ls.js">\r\n</script>\r\n\r\n<div class="book book_large" id="docassemble_container">\r\n    <div class="bookTabRow">\r\n        <ul class="tab_start_here htabs">\r\n        <li class="bookTabItem dynamicBookTabItem selected_book_tab">\r\n        <a class="select">Docassemble Interviews</a>\r\n        </li>\r\n        </ul>\r\n    </div>\r\n    <div class="bookPage">\r\n        <div class="dynamicBookContents tabs">\r\n                <div style="border: 0px; padding: 0px; margin: 0px;"><div class="form_container"><div class="listview_outer">\r\n                    <div id="interviews" class="datatable">\r\n                    </div>\r\n                </div></div></div>\r\n        </div>\r\n    </div>\r\n</div>\r\n```\r\n# Sending fields to Docassemble without displaying in Legal Server\r\nYou can create a whole tab block of fields that won\'t display in Legal Server but are still available to Docassemble,\r\nby creating an instruction with the contents:\r\n```\r\n<div id="docassemble-fields"></div>\r\n```\r\nSomewhere in the tab block that you want to hide.\r\n\r\n# Listviews\r\nAs of version .30, this module supports Legal Server Listviews. \r\nPlease check the ListView option "Show list title" in order to get labeled data into Docassemble. Every listview\r\nin the form section needs to be labeled for the module to be able to guess which label corresponds to which list view.\r\n\r\n# Filtering\r\n\r\nBy default, interviews will be filtered to match the metadata tag equalling the program name.\r\nAny interview tagged "everyone" will also be displayed.\r\n\r\n',
+      long_description_content_type='text/markdown',
+      author='Tom Brooke',
+      author_email='tmb@TrustBlocks.com',
+      license='The MIT License (MIT)',
+      url='https://github.com/TrustBlocks/docassemble-tb.git',
+      packages=find_packages(),
+      namespace_packages=['docassemble'],
+      install_requires=[],
+      zip_safe=False,
+      package_data=find_package_data(where='docassemble/tblocks/', package='docassemble.tblocks'),
+     )
+
